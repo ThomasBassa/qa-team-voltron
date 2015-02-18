cfae7d949f66188b4b03340f4f95e8d9a75d138c
Thomas Bassa <ThomasBassa@users.noreply.github.com>
2015-02-18 16:55:36 -0500

    Add information about Queue interface to custom one

--- Mini Project 02/application/FixedSizeQueueInterface.java
+++ Mini Project 02/application/FixedSizeQueueInterface.java
@@ -3,6 +3,16 @@ package application;
 import java.util.Queue;
 
 public interface FixedSizeQueueInterface<E> extends Queue<E> {
+	
+	/* Methods inherited from Queue<E>:
+	 * boolean add(E e) //return true if OK, throws IllegalStateException if no space
+	 * boolean offer(E e) //As add, without exception throwing if full (just return false)
+	 * E remove() //throws NoSuchElementException if empty queue
+	 * E poll() //returns null if empty queue
+	 * E element() //Returns, doesn't remove front. Throws NoSuchElementException if empty
+	 * E peek() //Returns front item or null when empty
+	 */
+	
 	/**
 	 * This method will obtain the maximum size for the queue, namely the number
 	 * of elements that can be held on the queue without it overflowing.
