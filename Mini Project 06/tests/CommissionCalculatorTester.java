e8a633a61610093215509d3bf535700ff513eb71
Thomas Bassa <ThomasBassa@users.noreply.github.com>
2015-03-24 23:18:16 -0400

    Add Mini Project 6 initial content.

---
+++ Mini Project 06/tests/CommissionCalculatorTester.java
@@ -0,0 +1,29 @@
+package tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/** Test suite for the Commission Calculator, as part of QA Mini Project 6.
+ * @author Thomas Bassa
+ * @author Greg Carkin
+ * @author Umar Idris
+ * @author Michael Philotoff */
+public class CommissionCalculatorTester {
+
+	/** Performs the following set up before each test
+	 * is executed:
+	 * TODO Do something!
+	 * @throws Exception on encountering an issue (shouldn't happen) */
+	@Before
+	public void setUp() throws Exception {
+		//Perform necessary setup before ALL tests here...
+	}
+
+	//For each test, Javadoc comment what the expected I/O is...
+	@Test
+	public void test() {
+		fail("No tests implemented yet.");
+	}
+}
