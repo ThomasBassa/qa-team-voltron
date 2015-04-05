a0e59b34e2ffdbf7d47a494286d66b4c7dc75232
Thomas Bassa <ThomasBassa@users.noreply.github.com>
2015-04-04 21:30:47 -0400

    Add JUnit test class.

---
+++ Mini Project 07/test/SecurityLightTester.java
@@ -0,0 +1,30 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+
+/** A test suite for the security light logic.
+ * @author Thomas Bassa
+ * @author Greg Carkin
+ * @author Umar Idris
+ * @author Michael Philotoff */
+public class SecurityLightTester {
+
+	/** Used to initialize the state of... something
+	 * before executing any test.
+	 * @throws Exception if something bad happens, hopefully not */
+	@Before
+	public void setUp() throws Exception {
+
+	}
+
+	//Let's write some tests!
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
